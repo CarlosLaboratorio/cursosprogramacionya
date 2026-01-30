ae74b180c1d1da7fa66e7395898327f748321bb4
Carlos Aguirre <carlosanibal815@gmail.com>
2026-01-30 11:31:19 -0300

    Componentes base

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ItemListContainer from './ItemListContainer';
+import NavBar from './NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer titulo="Cursos de React JS"/>
+    </>
+  );
+}
+
+export default App
