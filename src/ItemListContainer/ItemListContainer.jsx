c94a3716f04fde8f4d2688de85008d2e23ec0ec4
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-27 13:22:54 -0300

    usos useeffect usestate then map y key

--- src/ItemListContainer/ItemListContainer.jsx
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,44 @@
+import { useEffect,useState } from "react"
+import productsData from "../assets/products.json"
+import ItemCard from "../Item/ItemCard"
 import "./ItemListContainer.css"
 import {useParams} from 'react-router-dom'
 
 function ItemListContainer ({greeting}) {
 
     const {categoryId} = useParams()
+    const [products,setProducts] = useState([])
 
-    console.log(categoryId)
+    useEffect(()=>{
+        const getProducts = new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve(productsData)
+            },500)
+        })
+
+        getProducts.then((res)=>{
+            if (categoryId){
+                const filtered = res.filter(
+                    (prod) => prod.type.toLowerCase() === categoryId.toLowerCase()
+                )
+                setProducts(filtered)
+            }else{
+                setProducts(res)
+            }
+        })
+    },[categoryId])
 
     return (
-    <>
-        <h2 className="msj__subtitle">{greeting}</h2>
-        {categoryId && <p>Categoria seleccionada: {categoryId}</p>}
-    </>
+        <div className="container">
+            <h2 className="msj__subtitle">{greeting}</h2>
+            {categoryId && <p>Categoria seleccionada: {categoryId}</p>}
+            <div className="card-container">
+                {products.map((product)=>(
+                    <ItemCard key={product.id} product={product} />
+                ))
+                }
+            </div>
+        </div>
     )
 
 }
