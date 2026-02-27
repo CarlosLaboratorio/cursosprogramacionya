79127d115e84eecd58d46d709fdcf41792285930
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-27 00:42:17 -0300

    archivos del carrito

---
+++ src/CartWidget/CartWidget.jsx
@@ -0,0 +1,15 @@
+import "./CartWidget.css"
+
+function CartWidget () {
+    
+    return (
+        <div>
+            <img src="/img/cart.png" alt="Carrito de Compras" className="cuadro__carrito"/>
+        </div>
+    )
+
+
+
+}
+
+export default CartWidget
