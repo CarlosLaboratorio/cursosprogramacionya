5727c4e11b7e10897500058f133f2d2e93146ec4
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-27 11:45:40 -0300

    se agregan rutas

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {Routes,Route} from 'react-router-dom'
 import ItemListContainer from '../ItemListContainer/ItemListContainer';
 import NavBar from '../NavBar/NavBar';
 
@@ -6,6 +7,13 @@ function App() {
   return (
     <>
       <NavBar />
+
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting="Todos los productos"/>} />
+        <Route path='/category/:categoryId' element={<ItemListContainer greeting="Productos por Categoria"/>} />
+        <Route path='/item/:itemId' element={<h2 className='msj__subtitle'>Detalle del Producto</h2>} />
+      </Routes>
+
       <ItemListContainer greeting="Cursos de React JS"/>
     </>
   );
