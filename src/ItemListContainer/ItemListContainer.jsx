c67edb9dcbf20575f437a58e55ed0362a59f7b23
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-27 00:45:23 -0300

    archivos itemlistcontainer

---
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import "./ItemListContainer.css"
+
+function ItemListContainer ({greeting}) {
+
+    return (
+    <>
+        <h2 className="msj__subtitle">{greeting}</h2>
+    </>
+    )
+
+}
+
+export default ItemListContainer
