eefbb14f76acf5fc7e35faa1bad46898ba583441
Carlos Aguirre <carlosanibal815@gmail.com>
2026-02-27 11:49:45 -0300

    leyendo parametros de las categorias

--- src/ItemListContainer/ItemListContainer.jsx
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,16 @@
 import "./ItemListContainer.css"
+import {useParams} from 'react-router-dom'
 
 function ItemListContainer ({greeting}) {
+
+    const {categoryId} = useParams()
+
+    console.log(categoryId)
 
     return (
     <>
         <h2 className="msj__subtitle">{greeting}</h2>
+        {categoryId && <p>Categoria seleccionada: {categoryId}</p>}
     </>
     )
 
